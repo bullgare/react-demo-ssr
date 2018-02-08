7d905613e6a83da1a2080118f775fa40a79cd1f8
bullgare <bullgare@gmail.com>
2018-02-08 22:07:30 +0300

    no routes yet

---
+++ src/client/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>The home component!12<button onClick={() => console.log('!1')}>click</button></div>
+    );
+};
+
+export default Home;
--- src/client/components/home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => {
-    return (
-        <div>The home component</div>
-    );
-};
-
-export default Home;
